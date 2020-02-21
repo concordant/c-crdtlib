ffb3ee0a89e9ff1d75caf55a7584b014d0508cba
Ludovic Le Frioux <llefriou@po461-pro.paris.inria.fr>
2020-02-21 14:20:00 +0100

    Correct indentation

--- src/main/kotlin/crdtlib/GenerateTS.kt
+++ src/main/kotlin/crdtlib/GenerateTS.kt
@@ -8,19 +8,19 @@ import crdtlib.crdt.*
 fun main(args: Array<String>) {
     println(TypeScriptGenerator(
         rootClasses = setOf(
-        DCId::class,
-        Environment::class,
-        SimpleEnvironment::class,
-        Timestamp::class,
-        UnexpectedTypeException::class,
-        VersionVector::class,
-        DeltaCRDT::class,
-        Delta::class,
-        LWWRegister::class,
-        MVRegister::class,
-        PNCounter::class,
-        UpdateOperation::class,
-        EmptyDelta::class
+            DCId::class,
+            Environment::class,
+            SimpleEnvironment::class,
+            Timestamp::class,
+            UnexpectedTypeException::class,
+            VersionVector::class,
+            DeltaCRDT::class,
+            Delta::class,
+            LWWRegister::class,
+            MVRegister::class,
+            PNCounter::class,
+            UpdateOperation::class,
+            EmptyDelta::class
         )/*,
         mappings = mapOf(
             LocalDateTime::class to "Date",
