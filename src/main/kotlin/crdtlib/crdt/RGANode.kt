384c3eb83bbd56ab590c3e624188b108ca3c8882
Ludovic Le Frioux <ludovic.le-frioux@inria.fr>
2020-04-06 11:32:30 +0200

    Add comments to RGANode

--- src/main/kotlin/crdtlib/crdt/RGANode.kt
+++ src/main/kotlin/crdtlib/crdt/RGANode.kt
@@ -4,5 +4,10 @@ import crdtlib.utils.Timestamp
 
 /**
 * Data class representing information stored in an RGA node.
+* @property atom the atom stored within the node.
+* @property uid the unique identifier (timestamp) associated with the node.
+* @property anchor the uid of the node at the left of this node when it has been created.
+* @property ts the timestamp associated with last update of the node.
+* @property removed a boolean to know if the node is a tombstone.
 */
-data class RGANode(val atom: Char, val anchor: Timestamp?, val uid: Timestamp, val ts: Timestamp, val removed: Boolean)
+data class RGANode(val atom: Char, val uid: Timestamp, val anchor: Timestamp?, val ts: Timestamp, val removed: Boolean)
