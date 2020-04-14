a0379926173ba775edaba059a4e7992707e2e830
Ludovic Le Frioux <ludovic.le-frioux@inria.fr>
2020-04-14 11:17:22 +0200

    Comments and tests for Timestamp.

--- src/main/kotlin/crdtlib/utils/Timestamp.kt
+++ src/main/kotlin/crdtlib/utils/Timestamp.kt
@@ -1,6 +1,18 @@
 package crdtlib.utils
 
+/**
+* This class represents a timestamp.
+* @property id the datacenter id.
+* @property cnt the value associated to the timestamp.
+**/
 data class Timestamp(val id: DCId, val cnt: Int) : Comparable<Timestamp> {
+
+    /**
+    * Compares this timestamp to a given other timestamp.
+    * First comparison is made on their values and if equal on their datacenter ids.
+    * @param other the other instance of timestamp.
+    * @return the results of the comparison between the two timestamp.
+    **/
     override fun compareTo(other: Timestamp): Int {
         if(this.cnt != other.cnt)
             return this.cnt - other.cnt
