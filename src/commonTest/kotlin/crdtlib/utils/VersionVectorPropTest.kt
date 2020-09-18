26c9d80573641f1083cc81c727e5d2a91db7f361
Ludovic Le Frioux <llefriou@po461-pro.paris.inria.fr>
2020-09-18 14:12:59 +0200

    Use 'manual' xor because it is badly translated to javascript.

--- src/commonTest/kotlin/crdtlib/utils/VersionVectorPropTest.kt
+++ src/commonTest/kotlin/crdtlib/utils/VersionVectorPropTest.kt
@@ -28,21 +28,30 @@ class VersionVectorPropTest: StringSpec({
     "deserialize is inverse to serialize" {
         forAll(versionVectorArb) { vv ->
             val vv2 = VersionVector.fromJson(VersionVector(vv).toJson())
-            vv.isSmallerOrEquals(vv2)
-            vv2.isSmallerOrEquals(vv)
+            vv == vv2
         }
     }
+
+    "two vvs are either concurrent or comparable" {
+        forAll(versionVectorArb, versionVectorArb) { vv1, vv2 ->
+            val isComparable = vv1.isSmaller(vv2) || vv1.isGreater(vv2) || vv1 == vv2
+            val isNotComparable = vv1.isNotComparable(vv2) && vv2.isNotComparable(vv1)
+            (isComparable || isNotComparable) && (!isComparable || !isNotComparable)
+        }
+    }
+
     "copy generates equal version vector" {
         forAll(versionVectorArb) { vv1 ->
             val vv2 = vv1.copy()
-            vv2.isSmallerOrEquals(vv1) && vv1.isSmallerOrEquals(vv2)
+            vv1 == vv2
         }
     }
+
     "merge is idempotent" {
         forAll(versionVectorArb) { vv1 ->
             val vv2 = vv1.copy()
             vv2.pointWiseMax(vv1)
-            vv2.isSmallerOrEquals(vv1) && vv1.isSmallerOrEquals(vv2)
+            vv1 == vv2
         }
     }
     "merge is commutative" {
@@ -50,7 +59,7 @@ class VersionVectorPropTest: StringSpec({
             val vv2copy = vv2.copy()
             vv2.pointWiseMax(vv1)
             vv1.pointWiseMax(vv2copy)
-            vv2.isSmallerOrEquals(vv1) && vv1.isSmallerOrEquals(vv2)
+            vv1 == vv2
         }
     }
     "added timestamp should be included" {
@@ -73,6 +82,3 @@ class VersionVectorPropTest: StringSpec({
         }
     }
 })
-
-
-
