b72c059db8c67c6d36ccb492ffaa6226d30b5129
Ludovic Le Frioux <ludovic.le-frioux@inria.fr>
2020-10-29 12:57:25 +0100

    Reduce range for list size to avoid timeout in js test.

--- src/commonTest/kotlin/crdtlib/crdt/RatchetPropTest.kt
+++ src/commonTest/kotlin/crdtlib/crdt/RatchetPropTest.kt
@@ -74,8 +74,8 @@ class RatchetPropTest : StringSpec({
     }
 
     "merge with deltas" {
-        // Need to reduce range to 0..50 otherwise js test fails due to timeout (default is 0..100)
-        forAll(Arb.list(Arb.string(), 0..50), Arb.list(Arb.string(), 0..50)) { ops1, ops2 ->
+        // Need to reduce range to 0..25 otherwise js test fails due to timeout (default is 0..100)
+        forAll(Arb.list(Arb.string(), 0..25), Arb.list(Arb.string(), 0..25)) { ops1, ops2 ->
             val m1 = ops1.maxOrNull() ?: ""
             val m2 = ops2.maxOrNull() ?: ""
 
